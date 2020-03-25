6c6a2e1f2a026f4b313974d4ac63130f8b3d8d79
TJ Palanca <tj.palanca@firstcircle.com>
2020-03-25 01:55:58 +0000

    WIP migration

--- R/initial-blog-migration.R
+++ R/initial-blog-migration.R
@@ -222,7 +222,8 @@ open_post <- function() {
   file.edit(new_post_loc)
 }
 
-post_number <- 40
+nrow(post_data)
+post_number <- 44
 post_data$old_post[post_number]
 open_post()
 copy_image("img/posts/20130612-turkey-bloody-friday.jpg")
