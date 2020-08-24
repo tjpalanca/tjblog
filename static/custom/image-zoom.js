28854a598a3411daf8a312d002e7db38432d0dcc
TJ Palanca <tj.palanca@firstcircle.com>
2020-08-24 04:18:36 +0000

    Remove click behavior from images in post lists

--- static/custom/image-zoom.js
+++ static/custom/image-zoom.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  $('.d-article :not(.posts-list) img').on('click', function() {
+  $('.d-article img').on('click', function() {
     window.open($(this).attr('src'));
   });
+  $('.d-article .posts-list img').off('click');
 });
